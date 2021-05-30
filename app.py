72bd5caf5ee59dcf24db82638d35be88ecc94225
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2021-05-30 08:53:52 -0400

    Implement clean_data and balance_teams functions

--- app.py
+++ app.py
@@ -1,46 +1,48 @@
 """Basketball stats application"""
-# import PLAYERS, TEAMS from constants
+from constants import PLAYERS, TEAMS
 
-# function clean_data
-# with parameter players
-    # define cleaned_players, set to empty list
 
-    # for each player in players
-        # define cleaned_player, set to empty dictionary
-        # for each key/value pair in player
-            # define cleaned_value, set to value
-            # if key is height
-                # set cleaned_value to result of coercing value to integer
-            # else if key is experience
-                # set cleaned_value to result of if value is equal to YES
-            # end if
+def clean_data(players):
+    cleaned_players = []
 
-            # add key of key to cleaned_player, setting value to cleaned_value
-        # end for
+    for player in players:
+        cleaned_player = {}
 
-        # append cleaned_player to list cleaned_players
-    # end for
+        for key, value in player.items():
+            cleaned_value = value
 
-    # return cleaned_players
-# end function
+            if key == "height":
+                cleaned_value = int(value[0:2])
+            elif key == "experience":
+                cleaned_value = value == "YES"
 
-# function balance_teams
-# with parameter cleaned_players
-# and parameter teams
-    # define players_per_team, set to quotient of length of cleaned_players list and length of teams list
-    # define teams_with_players, set to empty dictionary
+            cleaned_player[key] = cleaned_value
 
-    # for each index, team in enumeration of teams
-        # define start, set to product of index and players_per_team
-        # define end, set to sum of start and players_per_team
+        cleaned_players.append(cleaned_player)
 
-        # define players_on_team, set to slice of cleaned_players from start to end
+    return cleaned_players
 
-        # add key of team to teams_with_players, setting value to players_on_team
-    # end for
 
-    # return teams_with_players
-# end function
+def balance_teams(cleaned_players, teams):
+    players_per_team = len(cleaned_players) // len(teams)
+    teams_with_players = {}
+
+    for index, team in enumerate(teams):
+        start = index * players_per_team
+        end = start + players_per_team
+
+        players_on_team = cleaned_players[start:end]
+
+        teams_with_players[team] = players_on_team
+
+    return teams_with_players
+
 
 if __name__ == '__main__':
-    pass
+    cleaned_players = clean_data(PLAYERS)
+    teams_with_players = balance_teams(cleaned_players, TEAMS)
+
+    for team, players in teams_with_players.items():
+        print(team)
+        for player in players:
+            print(player)
